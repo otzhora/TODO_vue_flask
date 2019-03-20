33195f6d3ff53f65ec31ef94222e88e257452158
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-03-20 22:41:13 +0300

    simple todo api

--- api/app.py
+++ api/app.py
@@ -1,6 +1,29 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+from todo import TODO_manager
+todo_manager = TODO_manager()
+
+from flask import jsonify
+from flask import request
+
+@app.route('/api')
 def main():
-    return 'Hello World'
+    return jsonify(todo_manager.todos)
+
+
+@app.route('/api/<id>', methods=['GET', 'POST', 'DELETE'])
+def manage_todos(id=None):
+    if request.method == 'GET':
+        return todo_manager.todos[id]
+    if request.method == 'POST':
+        todo_manager.add(id, request.form['text'])
+        return 'OK'
+    if request.method == 'DELETE':
+        todo_manager.delete(id)
+        return 'OK'
+    return 'NOT OK'
+
+    
+if __name__ == '__main__':
+    app.run(port=5001)
--- api/todo.py
+++ api/todo.py
@@ -1,11 +1,10 @@
-
-
 class TODO_manager:
-    def init(self):
-        self.todos = {}
+    def __init__(self):
+        self.todos = {'1':'test'}
+        print('initialized')
 
-    def add(self, todo):
-        self.todos[todo.id] = todo.body
+    def add(self, todo_id, todo):
+        self.todos[todo_id] = todo
 
     def delete(self, todo_id):
         del self.todos[todo_id]

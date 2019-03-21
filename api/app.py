cef9c5c5b632ca8cb32b112243a1866b5e9bffa4
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-03-21 23:37:53 +0300

    hosting front on server

--- api/app.py
+++ api/app.py
@@ -1,11 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_cors import CORS
+app = Flask(__name__, static_url_path="")
+CORS(app)
 
 from todo import TODO_manager
 todo_manager = TODO_manager()
 
 from flask import jsonify
 from flask import request
+from flask import send_from_directory
+from flask import render_template
+
+import os
+
 
 @app.route('/api')
 def main():
@@ -25,5 +32,29 @@ def manage_todos(id=None):
     return 'NOT OK'
 
     
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    print(os.getcwd() + '/dist/css' + path)
+    return send_from_directory(os.getcwd() + '/dist/css', path)
+
+
+@app.route('/')
+def send_index():
+    print(os.getcwd())
+    print(os.getcwd() + '/dist/')
+    return send_from_directory(os.getcwd() + '/dist/', 'index.html')
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory(os.getcwd() + '/dist/js',  path)
+
+
+@app.route('/img/<path:path>')
+def send_img(path):
+    return send_from_directory(os.getcwd() + '/dist/img',  path)
+
+
 if __name__ == '__main__':
     app.run(port=5001)
